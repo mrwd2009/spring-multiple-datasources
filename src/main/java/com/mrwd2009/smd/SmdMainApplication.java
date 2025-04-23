c79ac0ebfa547f3796b96e586a270acd39eea98e
Derek Wu <wudi.link.me@gmail.com>
2025-04-23 15:47:14 +0800

    init

---
+++ src/main/java/com/mrwd2009/smd/SmdMainApplication.java
@@ -0,0 +1,13 @@
+package com.mrwd2009.smd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrwd2009/smd/SmdMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrwd2009.smd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmdMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
