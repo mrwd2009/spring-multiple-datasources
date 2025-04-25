2f3937b4374c2fc3386074ade5e4bbfa9b51e937
Derek Wu <wudi.link.me@gmail.com>
2025-04-25 16:44:47 +0800

    Inject EntityManager.

--- src/main/java/com/mrwd2009/smd/core/usecase/BaseCalculationUseCaseImpl.java
+++ src/main/java/com/mrwd2009/smd/core/usecase/BaseCalculationUseCaseImpl.java
@@ -1,16 +1,26 @@
 package com.mrwd2009.smd.core.usecase;
 
 import com.mrwd2009.smd.core.repository.contractmodel.DeftableRepository;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BaseCalculationUseCaseImpl implements BaseCalculationUseCase {
-    private DeftableRepository deftableRepository;
+    private final DeftableRepository deftableRepository;
+    private static final Log logger = LogFactory.getLog(BaseCalculationUseCaseImpl.class);
+    @PersistenceContext(unitName = "baseModelUnit")
+    private EntityManager entityManager;
     @Override
     public void calculate() {
         var def = deftableRepository.findById(10L);
+        var user = entityManager.createQuery("select b.id, b.version from BillingCycleEntity b").setMaxResults(1).getSingleResult();
+        logger.info("test logger");
+        System.out.println(user);
         System.out.println(def.get());
     }
 }
